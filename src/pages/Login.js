27ef8e9c2fb99fee5ab113df27f0854e3f8cd413
“Chris” <kowalskic4@gmail.comgit config --global user.name Chris Kowalskigit config --list --show-origin>
2026-03-25 12:47:44 -0500

    Login as Guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,6 +26,18 @@ export const Login = () => {
       }
     }
 
+    async function handleLoginGuest(){
+      email.current.value = "mytest@test.com";
+      password.current.value = "P@ssword";
+      try{
+      const data = await login({email: email.current.value, password: password.current.value});
+      data.accessToken ? navigate("/products") :  toast.error(data);
+        } catch(error) {
+            toast.error(error.message, {closeButton: true, position: "bottom-center"});
+      }
+
+    }
+
   return (
     <main>
       <section>
@@ -34,7 +46,7 @@ export const Login = () => {
         <form onSubmit={handleLogin}>
           <div className="mb-6">
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="shubham@example.com" required autoComplete="off" />
+              <input ref={email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your-email@example.com" required autoComplete="off" />
           </div>
           <div className="mb-6">
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
@@ -42,7 +54,7 @@ export const Login = () => {
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
         </form>
-        {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+        <button onClick={() => handleLoginGuest()} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   )
 }
